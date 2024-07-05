8a6fdd19a2b8a92906cda1c88cc40ec7ec72d127
sung <salamander011017@gmail.com>
2024-07-05 09:56:37 +0900

    [BOJ] 스카이라인 쉬운거 / 골드 4 / 1시간 https://www.acmicpc.net/problem/1863

--- leeYuSung/BOJ/BOJ1759.java
+++ leeYuSung/BOJ/BOJ1759.java
@@ -1,6 +1,6 @@
 package JavaCodingTestStudy.week4;
 
-import java.io.BufferedReader;
+import  java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
